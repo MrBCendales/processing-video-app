676d2b73d6acee542dd7c522a402d4b0bb007d3b
MrBCendales <bn.cendales10@gmail.com>
2024-07-12 18:04:58 +0900

    new changes

---
+++ src/storage.ts
@@ -0,0 +1,10 @@
+// 1. GCS Google cloud storage file interactions
+// 2. Local files interactions
+
+import { Storage } from "@google-cloud/storage";
+import fs from "fs";
+import ffmpeg from "fluent-ffmpeg";
+
+export function setupDirectories() {}
+
+export function convertVideo() {}
